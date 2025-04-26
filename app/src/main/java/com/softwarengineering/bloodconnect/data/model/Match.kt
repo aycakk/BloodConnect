8d876f7bc0705c1f8932bf1a025a6248cf5b730e
Onur Kılıç <onur.kilic.b@gmail.com>
2025-04-27 02:03:37 +0300

    Update Match.kt

--- app/src/main/java/com/softwarengineering/bloodconnect/data/model/Match.kt
+++ app/src/main/java/com/softwarengineering/bloodconnect/data/model/Match.kt
@@ -1,11 +1,12 @@
 package com.softwarengineering.bloodconnect.data.model
 
 data class Match(
-    var matchID: String = "",
+    //var matchID: String = "",
     var donorID: String = "",
     var donorName: String = "",
     var donorBloodType: String = "",
-    var recipientID: String = "",
-    var matchScore: Int = 0,  // Eşleşme oranı (%0 - %100)
-    var status: String = "matched"  // "matched", "pending", "completed" olabilir
+    var hospitalID: String = "",
+    var matchScore: Int = 0,
+    var neededBloodType: String = ""// Eşleşme oranı (%0 - %100)
+    //var status: String = "matched"  // "matched", "pending", "completed" olabilir
 )
